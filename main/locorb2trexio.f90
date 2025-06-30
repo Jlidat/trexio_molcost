ca916098d313b501d0c464f6e6fffe05bd1c89d7
Jlidat <njlidat@yahoo.com>
2025-06-30 16:34:03 +0200

    Codes versions finales

---
+++ main/locorb2trexio.f90
@@ -0,0 +1,108 @@
+program locorb2trexio
+        use trexio
+        implicit none
+        character(256) :: ifilename, ofilename
+
+        call copy_coef(ifilename, ofilename)
+contains
+     subroutine copy_coef(input_filename, output_filename)
+             use trexio
+        implicit none
+        integer(trexio_t) :: trexio_file
+        integer(trexio_exit_code) ::rc
+        character*(128) :: err_msg
+
+        character(*) :: input_filename, output_filename
+        integer :: input_unit , output_unit
+        
+        integer :: ios,i,j
+        integer :: mo_num, ao_num,pas
+        character(len=72)::line
+        real(8), allocatable:: mo_coefficient(:,:)
+        real(8) ::c1, c2, c3, c4, c5,c11, c22,c33,C111,C222
+
+!--------Nom des fichiers d'entrée et de sortie
+       ! input_filename='LOCORB'
+       call getarg(1, input_filename)
+!        output_filename='h2o.h5'
+        call getarg(2, output_filename)
+        input_unit=10
+        output_unit=11
+
+!-------Ouvrir le fichier input
+        open(unit=input_unit,file=input_filename,access='sequential',&
+                status='old',position='asis',iostat=ios)
+        if(ios/=0)then
+               print*, 'Erreur d''ouverture du fichier source', input_filename
+              stop
+        endif
+
+!-------Ouvrir trexio en mode écriture
+        trexio_file=trexio_open(output_filename,'u', TREXIO_HDF5, rc)
+        if(rc/=TREXIO_SUCCESS) then
+                call trexio_string_of_error(rc,err_msg)
+                print*,'Error: '//trim(err_msg)
+                call exit(-1)
+        endif
+!-------Lire le fichier LACORB ligne par ligne; les 5 lignes premieres ne sont pas écrites
+        do i=1, 4
+               read(input_unit,fmt='(A)')line
+              !read(line, fmt='(A)') temp 
+        enddo
+!-------lire le nombre d'orbitals atomiques
+        read(input_unit,fmt='(A)') line
+        read(line, fmt='(I8)') ao_num
+        !print *, 'ao_num=', ao_num
+!-------lire le nombre d'orbitals moléculaires
+        read(input_unit,fmt='(A)') line
+        read(line, fmt='(I8)') mo_num
+      !print *, 'mo_num=', mo_num 
+!-----------------------------------------------
+       read(input_unit,fmt='(A)') line
+      !print *, 'tempon2', line
+!-------pas=6----------------------------------------
+       pas=int(mo_num/4)
+!---------------------------------------------------------------------------------------------------
+        allocate(mo_coefficient(ao_num,mo_num))
+     do j=1, mo_num
+        read(input_unit,fmt= '(A72)')line
+        do i=1, pas
+             read(input_unit, fmt='(4(D18.12))') c1, c2, c3, c4 
+             mo_coefficient(1+(4*(i-1)),j)=c1
+             mo_coefficient(2+(4*(i-1)),j)=c2
+             mo_coefficient(3+(4*(i-1)),j)=c3
+             mo_coefficient(4+(4*(i-1)),j)=c4
+        enddo
+           if(mod(ao_num,4)/=0) then
+             if(mod(ao_num,4)==3)then
+                  read(input_unit, fmt='(3(D18.12))') c11, c22, c33 
+                  mo_coefficient(ao_num-2,j)=c11
+                  mo_coefficient(ao_num-1,j)=c22
+                  mo_coefficient(ao_num,j)=c33
+             elseif(mod(ao_num,4)==2)then
+                  read(input_unit, fmt='(2(D18.12))') c111, c222 
+                  mo_coefficient(ao_num-1,j)=c111
+                  mo_coefficient(ao_num,j)=c222
+             else
+                 read(input_unit, fmt='(D18.12)')c5
+                 mo_coefficient(ao_num,j)=c5
+              endif
+           endif
+     enddo
+!-----affichage de mo_coefficient avant écriture dans Trexio------------     
+!     do j=1, mo_num
+!        print '(D18.12)', (mo_coefficient(i,j), i=1,ao_num)
+!     enddo
+!---------------------------------------------------------------------------------------------------
+        rc=trexio_write_mo_coefficient(trexio_file,mo_coefficient)
+        if (rc/=TREXIO_SUCCESS) then 
+                call trexio_string_of_error(rc, err_msg)
+                print *, 'Error: '//trim(err_msg)
+                call exit(-1)
+        endif
+
+!---------------------------------------------------------------------------------------------------
+       rc=trexio_close(trexio_file)
+       close(input_unit)
+    end
+end
---
+++ main/script.f
@@ -0,0 +1,150 @@
+      SUBROUTINE script (OAP,M,N,MD,ND)
+      IMPLICIT REAL*8(A-H,O-Z)
+      DIMENSION OAP(MD,ND)
+      nnn=10
+      NN=N/nnn
+      IF(NN.EQ.0) GOTO 85
+      DO 83 KK=1,NN
+      N1=nnn*(KK-1)+1
+      N2=nnn*KK
+      IF(N2.LT.N1) RETURN
+      print*,' '
+      WRITE(6,1024) (I,I=N1,N2)
+      print*,' '
+      DO 83 JJ=1,M
+83    WRITE(6,1025) JJ,(OAP(JJ,II),II=N1,N2)
+      print*,' '
+85    N1=nnn*NN+1
+      N2=N
+      IF(N2.LT.N1) RETURN
+      print*,' '
+      WRITE(6,1024) (I,I=N1,N2)
+      print*,' '
+      DO 84 JJ=1,M
+84    WRITE(6,1025) JJ,(OAP(JJ,II),II=N1,N2)
+      print*,' '
+1024  FORMAT(1X,10(3X,I5))
+1025  FORMAT(I5,10F8.4)
+      RETURN
+      END
+       subroutine scri1(nsym,r8,isym)
+      IMPLICIT DOUBLE PRECISION (A-H,O-Z)
+      dimension isym(8),r8(*)
+      idep=1
+      do k=1,nsym
+         call scri8(r8(idep),isym(k))
+         idep=idep+isym(k)*(isym(k)+1)/2
+      enddo
+      return
+      end
+       subroutine scri8(r8,n)
+      IMPLICIT DOUBLE PRECISION (A-H,O-Z)
+      dimension r8(*)
+      ifin=0
+      do k=1,n
+      ideb=ifin+1
+      ifin=ideb+k-1
+      write(6,'(10f8.4)')(r8(kk),kk=ideb,ifin)
+      enddo
+      return
+      end
+       SUBROUTINE scripi (mat,M,N,MD,ND,iform)
+      IMPLICIT REAL*8(A-H,O-Z)
+      character*20 f1025,f1024
+      DIMENSION mat(MD,ND)
+      nnn=80/iform
+      if(iform.lt.10) then
+         write(f1024,'(''(4X,'',i2,''i'',i1,'')'')') nnn,iform
+         write(f1025,'(''(I4,'',i2,''i'',i1,'')'')') nnn,iform
+      else
+         write(f1024,'(''(4X,'',i2,''i'',i2,'')'')') nnn,iform
+         write(f1025,'(''(I4,'',i2,''i'',i2,'')'')') nnn,iform
+      endif
+      print*,'f1024',f1024
+      print*,'f1025',f1025
+c      f1024='(3X,10I8)'
+c      f1025='(i4,10I8)'
+c      nnn=10
+      NN=N/nnn
+      IF(NN.EQ.0) GOTO 85
+      DO 83 KK=1,NN
+      N1=nnn*(KK-1)+1
+      N2=nnn*KK
+      IF(N2.LT.N1) RETURN
+c      WRITE(6,1023)
+      WRITE(6,f1024) (I,I=N1,N2)
+c      WRITE(6,1022)
+      DO 83 JJ=1,M
+83    WRITE(6,f1025) JJ,(mat(JJ,II),II=N1,N2)
+c      WRITE(6,1022)
+85    N1=nnn*NN+1
+      N2=N
+      IF(N2.LT.N1) RETURN
+      WRITE(6,f1024) (I,I=N1,N2)
+      DO 84 JJ=1,M
+84    WRITE(6,f1025) JJ,(mat(JJ,II),II=N1,N2)
+c      WRITE(6,1022)
+c1022  FORMAT(/)
+c1023  FORMAT(/)
+c1024  FORMAT(4X,10(6X,I2))
+c1025  FORMAT(3X,I2,10i8)
+      RETURN
+      END 
+      SUBROUTINE script6 (OAP,M,N,MD,ND)
+      IMPLICIT REAL*8(A-H,O-Z)
+      DIMENSION OAP(MD,ND)
+      nnn=10
+      NN=N/nnn
+      IF(NN.EQ.0) GOTO 85
+      DO 83 KK=1,NN
+      N1=nnn*(KK-1)+1
+      N2=nnn*KK
+      IF(N2.LT.N1) RETURN
+      print*,' '
+      WRITE(6,1024) (I,I=N1,N2)
+      print*,' '
+      DO 83 JJ=1,M
+83    WRITE(6,1025) JJ,(OAP(JJ,II),II=N1,N2)
+      print*,' '
+85    N1=nnn*NN+1
+      N2=N
+      IF(N2.LT.N1) RETURN
+      print*,' '
+      WRITE(6,1024) (I,I=N1,N2)
+      print*,' '
+      DO 84 JJ=1,M
+84    WRITE(6,1025) JJ,(OAP(JJ,II),II=N1,N2)
+      print*,' '
+1024  FORMAT(1X,10(3X,I7))
+1025  FORMAT(I5,10F10.6)
+      RETURN
+      END
+       subroutine scritri(h,n)
+      IMPLICIT REAL*8(A-H,O-Z)
+      dimension h(n*(n+1)/2)
+      nn=n*(n+1)/2
+      ifin=0
+       print*,'n',n
+      do m=1,n,10
+         print*,' '
+         print'(4x,10i8)',(k,k=m,m+10-1)
+         print*,' '
+         ideb=ifin+1
+         ifin=ifin+10
+         jfin=(m+1)*m/2-1
+         do i=m,min(m+10-1,n)
+            jdeb=jfin+1
+            jfin=jfin+i-m+1
+            print'(i4,10f8.4)',i,(h(k),k=jdeb,jfin)
+            jfin=i*(i+1)/2+m-1
+         enddo
+         do i=m+10,n
+            jdeb=jfin+1
+            jfin=min(jfin+10,nn)
+            !print*,'i',i,jdeb,jfin
+            print'(i4,10f8.4)',i,(h(k),k=jdeb,jfin)
+            jfin=i*(i+1)/2+m-1
+         enddo
+      enddo
+      return
+      end
